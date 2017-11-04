f7f5f644c7344fd01f3ec554e879fd652932804f
Marc from GitKraken <marcmauri92@gmail.com>
2017-11-04 18:24:37 +0100

    Configurar y mostrar fragments base

---
+++ app/src/main/java/app/android/mmauri/seccion_06_fragments/Fragments/DataFragment.java
@@ -0,0 +1,32 @@
+package app.android.mmauri.seccion_06_fragments.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import app.android.mmauri.seccion_06_fragments.R;
+
+public class DataFragment extends Fragment {
+
+
+    public DataFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_data, container, false);
+
+
+
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+}
