0012823e952cb28fefbe0984bc3e682af7d4725a
Marc from GitKraken <marcmauri92@gmail.com>
2017-11-04 19:29:32 +0100

    Comunicacion entre fragments

--- app/src/main/java/app/android/mmauri/seccion_06_fragments/Activities/MainActivity.java
+++ app/src/main/java/app/android/mmauri/seccion_06_fragments/Activities/MainActivity.java
@@ -1,16 +1,27 @@
 package app.android.mmauri.seccion_06_fragments.Activities;
 
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
+import app.android.mmauri.seccion_06_fragments.Fragments.DataFragment;
+import app.android.mmauri.seccion_06_fragments.Fragments.DetailsFragment;
 import app.android.mmauri.seccion_06_fragments.R;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity implements DataFragment.DataListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void sendData(String text) {
+        /* Llamar al metodo renderizarTexto del DetailsFragment, pasando el texto que
+           recibimos por parametro en este mismo metodo */
+
+        DetailsFragment detailsFragment = (DetailsFragment) getSupportFragmentManager().findFragmentById(R.id.detailsFragment);
+        detailsFragment.renderText(text);
+
     }
 }
--- app/src/main/java/app/android/mmauri/seccion_06_fragments/Fragments/DetailsFragment.java
+++ app/src/main/java/app/android/mmauri/seccion_06_fragments/Fragments/DetailsFragment.java
@@ -6,10 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import app.android.mmauri.seccion_06_fragments.R;
 
 public class DetailsFragment extends Fragment {
+
+    private TextView textViewDetails;
 
 
     public DetailsFragment() {
@@ -22,11 +25,17 @@ public class DetailsFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_details, container, false);
+        // Rescatamos los objetos de la UI
 
+        textViewDetails = (TextView) view.findViewById(R.id.textViewDetails);
 
 
         // Inflate the layout for this fragment
         return view;
+    }
+
+    public void renderText(String text) {
+        textViewDetails.setText(text);
     }
 
 }
